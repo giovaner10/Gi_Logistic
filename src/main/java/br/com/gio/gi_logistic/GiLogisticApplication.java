13b11556232d1c6c7f5e805a996729504a48ae17
giovaner10 <giovanerr10@gmail.com>
2022-04-11 23:59:44 -0300

    implemntando o mysql

---
+++ src/main/java/br/com/gio/gi_logistic/GiLogisticApplication.java
@@ -0,0 +1,13 @@
+package br.com.gio.gi_logistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiLogisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiLogisticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gio/gi_logistic/GiLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gio.gi_logistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiLogisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
