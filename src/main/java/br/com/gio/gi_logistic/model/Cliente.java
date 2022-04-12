1b1122b373e92ffb17948757af721c50039ce270
giovaner10 <giovanerr10@gmail.com>
2022-04-12 20:26:43 -0300

    implemntando beans validations no model Cliente.java

--- src/main/java/br/com/gio/gi_logistic/model/Cliente.java
+++ src/main/java/br/com/gio/gi_logistic/model/Cliente.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @NoArgsConstructor
@@ -21,8 +24,18 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String nome;
+
+    @NotBlank
+    @Size(max = 60)
     private String email;
+
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 
 }
