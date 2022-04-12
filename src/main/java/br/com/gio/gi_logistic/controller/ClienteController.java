5862cf0919871a144e52e8ec0b5572ae54a73d3a
giovaner10 <giovanerr10@gmail.com>
2022-04-12 19:23:02 -0300

    implemntando o ClienteController

--- src/main/java/br/com/gio/gi_logistic/controller/ClienteController.java
+++ src/main/java/br/com/gio/gi_logistic/controller/ClienteController.java
@@ -1,25 +1,57 @@
 package br.com.gio.gi_logistic.controller;
 
 import br.com.gio.gi_logistic.model.Cliente;
+import br.com.gio.gi_logistic.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/clientes")
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    //@PersistenceContext
+    // private EntityManager manager;
 
-    @GetMapping("/cli")
+    private ClienteRepository repository;
+
+    /*@GetMapping("/clientes")
     public List<Cliente> listar(){
 
-        return manager.createNativeQuery("select * from cliente", Cliente.class).getResultList();
+        //return manager.createNativeQuery("select * from cliente", Cliente.class).getResultList();
 
+        return repository.findAll();
+    }*/
+
+    @GetMapping
+    public List<Cliente> listarPorNome() {
+
+        //return manager.createNativeQuery("select * from cliente", Cliente.class).getResultList();
+
+        return repository.findByNomeContaining("novo");
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Integer clienteId) {
+
+        /*Optional<Cliente> cliente = repository.findById(clienteId);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();*/
+
+        return repository.findById(clienteId)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                        .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/br/com/gio/gi_logistic/repository/ClienteRepository.java
+++ src/main/java/br/com/gio/gi_logistic/repository/ClienteRepository.java
@@ -4,6 +4,10 @@ import br.com.gio.gi_logistic.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    List<Cliente> findByNomeContaining(String nome);
 }
