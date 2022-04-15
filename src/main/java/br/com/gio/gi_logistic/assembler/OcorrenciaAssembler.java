959abdcbab30eec108bcbb85c1538c3d5308b4ac
giovaner10 <giovanerr10@gmail.com>
2022-04-14 21:45:16 -0300

    implantando as Ocorrencia.java nas Entrega.java

--- src/main/java/br/com/gio/gi_logistic/assembler/OcorrenciaAssembler.java
+++ src/main/java/br/com/gio/gi_logistic/assembler/OcorrenciaAssembler.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 @Component
 public class OcorrenciaAssembler {
@@ -14,6 +17,10 @@ public class OcorrenciaAssembler {
 
     public OcorrenciaModel toEntity(Ocorrencia ocorrencia){
         return modelMapper.map(ocorrencia, OcorrenciaModel.class);
+    }
+
+    public List<OcorrenciaModel> toList(List<Ocorrencia> ocorrencias){
+        return ocorrencias.stream().map(this::toEntity).collect(Collectors.toList());
     }
 
 }
--- src/main/java/br/com/gio/gi_logistic/controller/OcorrenciaController.java
+++ src/main/java/br/com/gio/gi_logistic/controller/OcorrenciaController.java
@@ -1,15 +1,18 @@
 package br.com.gio.gi_logistic.controller;
 
 import br.com.gio.gi_logistic.assembler.OcorrenciaAssembler;
+import br.com.gio.gi_logistic.model.Entrega;
 import br.com.gio.gi_logistic.model.Ocorrencia;
 import br.com.gio.gi_logistic.model.input.OcorrenciaInput;
 import br.com.gio.gi_logistic.model.input.OcorrenciaModel;
+import br.com.gio.gi_logistic.service.BuscarEntregaService;
 import br.com.gio.gi_logistic.service.RegistroOcorrenciaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -18,6 +21,10 @@ public class OcorrenciaController {
 
     private RegistroOcorrenciaService registroOcorrenciaService;
     private OcorrenciaAssembler ocorrenciaAssembler;
+    private BuscarEntregaService buscarEntregaService;
+
+
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OcorrenciaModel registrar(@PathVariable Integer entregaId,
@@ -26,7 +33,15 @@ public class OcorrenciaController {
         Ocorrencia ocorrenciaRegistrada = registroOcorrenciaService
                 .registrar(entregaId, ocorrenciaInput.getDescricao());
 
-
         return ocorrenciaAssembler.toEntity(ocorrenciaRegistrada);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<OcorrenciaModel> Listar(@PathVariable Integer entregaId){
+
+        Entrega entrega  = buscarEntregaService.buscar(entregaId);
+
+        return  ocorrenciaAssembler.toList(entrega.getOcorrencias());
     }
 }
